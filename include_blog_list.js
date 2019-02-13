129bf2451ab16a44fc31bdd7e02b531f9314e8fb
Matt Lehman <mhlehman24@gmail.com>
2019-02-12 22:05:01 -0500

    Delete include_blog_list.js

--- include_blog_list.js
+++
@@ -1,9 +0,0 @@
-const xhr = new XMLHttpRequest();
-xhr.onload = function() {
-  let emptyDiv = document.getElementById("empty-div");
-  let body = this.responseXML.body;
-  emptyDiv.innerHTML = this.responseXML.body.innerHTML;
-}
-xhr.open("GET", "/mattle24_blog/public/index.html");
-xhr.responseType = "document";
-xhr.send();
